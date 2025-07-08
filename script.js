b9c6f085f90d4433e5adb894775d57b00f1a8e7b
Rafael Augusto <rafaelmiguel01@outlook.com>
2025-07-07 22:10:55 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,7 @@
+let botao=document.querySelector("#botao")
+let mensagem=document.querySelector("#mensagem")
+
+botao.addEventListener("click", function() {
+    mensagem.innerHTML="Bem-vindo, aqui você vai conhecer um pouco de mim!";
+});
+
